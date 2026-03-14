91040a79eaae557b5d02fb08c36e3fd4c5e5e06c
River Roseveare-Hunt <93801753+DogmaHG@users.noreply.github.com>
2026-03-13 21:48:39 -0700

    Update __init__.py

--- src/models/staging/__init__.py
+++ src/models/staging/__init__.py
@@ -4,12 +4,18 @@ Import all staging models here so Alembic autogenerate picks them up.
 """
 
 from src.models.staging.audit import StgAuditLineRaw
+from src.models.staging.auth_logs import RiverAuthLogStaging
+from src.models.staging.dns_events import RiverDnsEventsStaging
 from src.models.staging.host import StgHostLogConfigRaw, StgHostRaw
 from src.models.staging.labels import StgAttackLabelLineRaw
+from src.models.staging.vpn_events import RiverVpnEventsStaging
 
 __all__ = [
     "StgHostRaw",
     "StgHostLogConfigRaw",
     "StgAuditLineRaw",
     "StgAttackLabelLineRaw",
+    "RiverAuthLogStaging",
+    "RiverDnsEventsStaging",
+    "RiverVpnEventsStaging",
 ]
