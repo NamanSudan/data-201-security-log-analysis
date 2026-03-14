d4a7f38c147eb3f9ae7c95b3e754c5630558bce6
Ishaanshetty <ishaan2shetty@gmail.com>
2026-03-13 21:51:56 -0700

    fix class name IshaanHttpAccessStaging

--- src/models/staging/__init__.py
+++ src/models/staging/__init__.py
@@ -6,7 +6,7 @@ Import all staging models here so Alembic autogenerate picks them up.
 from src.models.staging.audit import StgAuditLineRaw
 from src.models.staging.host import StgHostLogConfigRaw, StgHostRaw
 from src.models.staging.http_events_staging import IshaanApacheErrorStaging
-from src.models.staging.ishaan_http_access_staging import IshaanApacheAccessStaging
+from src.models.staging.ishaan_http_access_staging import IshaanHttpAccessStaging
 from src.models.staging.labels import StgAttackLabelLineRaw
 from src.models.staging.system_cpu_events_staging import IshaanSystemCpuStaging
 
@@ -15,7 +15,7 @@ __all__ = [
     "StgHostLogConfigRaw",
     "StgAuditLineRaw",
     "StgAttackLabelLineRaw",
-    "IshaanApacheAccessStaging",
+    "IshaanHttpAccessStaging",
     "IshaanApacheErrorStaging",
     "IshaanSystemCpuStaging",
 ]
