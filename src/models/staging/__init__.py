66a9f5c38b313f264f23a3cf65b7e569bf1c12d9
Ishaanshetty <ishaan2shetty@gmail.com>
2026-03-13 21:44:38 -0700

    fix import paths in staging __init__.py

--- src/models/staging/__init__.py
+++ src/models/staging/__init__.py
@@ -5,9 +5,9 @@ Import all staging models here so Alembic autogenerate picks them up.
 
 from src.models.staging.audit import StgAuditLineRaw
 from src.models.staging.host import StgHostLogConfigRaw, StgHostRaw
-from src.models.staging.http_access_events_staging import IshaanApacheAccessStaging
-from src.models.staging.http_events_staging import IshaanApacheErrorStaging
 from src.models.staging.labels import StgAttackLabelLineRaw
+from src.models.staging.ishaan_http_access_staging import IshaanApacheAccessStaging
+from src.models.staging.http_events_staging import IshaanApacheErrorStaging
 from src.models.staging.system_cpu_events_staging import IshaanSystemCpuStaging
 
 __all__ = [
