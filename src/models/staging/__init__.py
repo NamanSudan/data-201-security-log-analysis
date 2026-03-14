8fb6465dca4a4a0ea9c056bc8dbe1ccab41d8451
Ishaanshetty <ishaan2shetty@gmail.com>
2026-03-13 21:37:14 -0700

    Update __init__.py

--- src/models/staging/__init__.py
+++ src/models/staging/__init__.py
@@ -18,18 +18,4 @@ __all__ = [
     "IshaanApacheAccessStaging",
     "IshaanApacheErrorStaging",
     "IshaanSystemCpuStaging",
-"""Staging table ORM models.
-
-Import all staging models here so Alembic autogenerate picks them up.
-"""
-
-from src.models.staging.audit import StgAuditLineRaw
-from src.models.staging.host import StgHostLogConfigRaw, StgHostRaw
-from src.models.staging.labels import StgAttackLabelLineRaw
-
-__all__ = [
-    "StgHostRaw",
-    "StgHostLogConfigRaw",
-    "StgAuditLineRaw",
-    "StgAttackLabelLineRaw",
 ]
