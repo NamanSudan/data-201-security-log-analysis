c692209ac995bf84b91c47030037548b1fcf4ef9
Ishaanshetty <ishaan2shetty@gmail.com>
2026-03-14 18:29:39 -0700

    Update __init__.py

--- src/models/staging/__init__.py
+++ src/models/staging/__init__.py
@@ -3,11 +3,18 @@
 Import all staging models here so Alembic autogenerate picks them up.
 """
 
-from src.models.staging.http_access import StgHttpAccess
-from src.models.staging.http_errors import StgHttpErrors
-from src.models.staging.system_cpu import StgSystemCpuEvents
+from src.models.staging.audit import StgAuditLineRaw
+from src.models.staging.host import StgHostLogConfigRaw, StgHostRaw
+from src.models.staging.labels import StgAttackLabelLineRaw
+from src.models.staging.http_apache_error import StgHttpErrors
+from src.models.staging.http_apache_access import StgHttpAccess
+from src.models.staging.system_cpu_internal import StgSystemCpuEvents
 
 __all__ = [
+    "StgHostRaw",
+    "StgHostLogConfigRaw",
+    "StgAuditLineRaw",
+    "StgAttackLabelLineRaw",
     "StgHttpErrors",
     "StgHttpAccess",
     "StgSystemCpuEvents",
