80a8c78a86ed9e4fa94983bf2c301fd5602ec431
Ishaanshetty <ishaan2shetty@gmail.com>
2026-03-15 20:46:04 -0700

    Ruff errors

--- alembic/versions/20260315_2020_d9e63582e3e2_create_audit_3nf_tables.py
+++ alembic/versions/20260315_2020_d9e63582e3e2_create_audit_3nf_tables.py
@@ -6,17 +6,18 @@ Create Date: 2026-03-15 20:20:47.684847
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
+from alembic import op
+
 # revision identifiers, used by Alembic.
 revision: str = "d9e63582e3e2"
-down_revision: Union[str, None] = "358010f5f4cc"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = "358010f5f4cc"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
