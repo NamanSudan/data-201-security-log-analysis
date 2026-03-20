1ba3b4a483df48f0b1775568e2c4090090818f82
River Roseveare-Hunt <93801753+DogmaHG@users.noreply.github.com>
2026-03-19 20:49:56 -0700

    Updated one Query to FULL OUTER JOIN

--- sql/queries/basic/river_attack_labels_per_host.sql
+++ sql/queries/basic/river_attack_labels_per_host.sql
@@ -1,6 +1,6 @@
 SELECT ap.phase_name,
        COUNT(al.label_id) AS label_count
 FROM attack_phase ap
-LEFT JOIN attack_label al ON al.phase_id = ap.phase_id
+FULL OUTER JOIN attack_label al ON al.phase_id = ap.phase_id
 GROUP BY ap.phase_id, ap.phase_name
 ORDER BY label_count DESC;
